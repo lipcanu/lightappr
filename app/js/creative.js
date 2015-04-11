35e22b6880ecdb21591c17713fd62071dbfafa56
lipcanu <diana.lipcanu@gmail.com>
2015-04-11 20:14:39 +0200

    carousel & quotes working

--- app/js/creative.js
+++ app/js/creative.js
@@ -3,10 +3,8 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-
 (function($) {
     "use strict"; // Start of use strict
-
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
@@ -15,34 +13,45 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-
     // Highlight the top nav as scrolling occurs
     $('body').scrollspy({
         target: '.navbar-fixed-top',
         offset: 51
     })
-
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
         $('.navbar-toggle:visible').click();
     });
-
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
-        1.2, {
-            minFontSize: '35px',
-            maxFontSize: '65px'
-        }
-    );
-
+    $("h1").fitText(1.2, {
+        minFontSize: '35px',
+        maxFontSize: '65px'
+    });
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
             top: 100
         }
     })
-
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
-
+    $('#myCarousel').carousel({
+        interval: 4000
+    });
+    // handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click(function() {
+        var id_selector = $(this).attr("id");
+        var id = id_selector.substr(id_selector.length - 1);
+        id = parseInt(id);
+        $('#myCarousel').carousel(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $(this).addClass('selected');
+    });
+    // when the carousel slides, auto update
+    $('#myCarousel').on('slid', function(e) {
+        var id = $('.item.active').data('slide-number');
+        id = parseInt(id);
+        $('[id^=carousel-selector-]').removeClass('selected');
+        $('[id=carousel-selector-' + id + ']').addClass('selected');
+    });
 })(jQuery); // End of use strict
